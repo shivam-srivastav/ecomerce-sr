ff6cea9f907a4e45717054d1c440117cd5225300
shivam1534 <shivam.mkp@gmail.com>
2021-03-10 13:26:23 +0530

    ecomerce-code

--- db/index.js
+++ db/index.js
@@ -1,10 +1,13 @@
 const mongoose = require("mongoose");
 mongoose
-  .connect("mongodb+srv://beast1534:dEoXOk37HDjzhvQ6@cluster0.kellf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  })
-  .then(() => console.log("Database for Automatic Attendence System is connected"))
+  .connect(
+    "mongodb+srv://beast1534:dEoXOk37HDjzhvQ6@cluster0.kellf.mongodb.net/ecommercesr?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("Database for E-commerce is connected"))
   .catch((err) => {
     console.error("Connection Error", err.message);
   });
